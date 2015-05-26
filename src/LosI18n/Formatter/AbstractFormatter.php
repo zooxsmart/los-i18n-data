f7f75d6d3646a5d830b1e6ac27f4ddfbd5150221
Leandro Silva <leandro@leandrosilva.info>
2015-05-26 20:38:20 -0300

    cleanup

--- src/LosI18n/Formatter/AbstractFormatter.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace LosI18n\Formatter;
-
-abstract class AbstractFormatter implements FormatterInterface
-{
-
-    abstract public function format(array $data);
-    abstract public function getExtension();
-}
--- src/LosI18n/Formatter/FormatterInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace LosI18n\Formatter;
-
-interface FormatterInterface
-{
-
-    public function format(array $data);
-}
